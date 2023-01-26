4a22f41af03102894676c2931cf08cc4fa7bf4c6
Tamo <tamo@meilisearch.com>
2023-01-26 18:32:03 +0100

    test the skip attribute

---
+++ tests/skip.rs
@@ -0,0 +1,149 @@
+use deserr::{deserialize, serde_json::JsonError, DeserializeFromValue};
+use insta::{assert_debug_snapshot, assert_display_snapshot};
+use serde_json::json;
+
+#[test]
+fn skip() {
+    #[allow(unused)]
+    #[derive(Debug, DeserializeFromValue)]
+    struct Struct {
+        #[deserr(skip)]
+        doggo: Option<String>,
+    }
+
+    let data = deserialize::<Struct, _, JsonError>(json!({})).unwrap();
+
+    assert_debug_snapshot!(data, @r###"
+    Struct {
+        doggo: None,
+    }
+    "###);
+
+    let data = deserialize::<Struct, _, JsonError>(json!({ "doggo": null })).unwrap();
+
+    assert_debug_snapshot!(data, @r###"
+    Struct {
+        doggo: None,
+    }
+    "###);
+
+    let data = deserialize::<Struct, _, JsonError>(json!({ "doggo": "bork" })).unwrap();
+
+    assert_debug_snapshot!(data, @r###"
+    Struct {
+        doggo: None,
+    }
+    "###);
+}
+
+#[test]
+fn skip_and_deny_unknown_fields() {
+    #[allow(unused)]
+    #[derive(Debug, DeserializeFromValue)]
+    #[deserr(deny_unknown_fields)]
+    struct Struct {
+        #[deserr(skip)]
+        doggo: Option<String>,
+    }
+
+    let data = deserialize::<Struct, _, JsonError>(json!({})).unwrap();
+
+    assert_debug_snapshot!(data, @r###"
+    Struct {
+        doggo: None,
+    }
+    "###);
+
+    let data = deserialize::<Struct, _, JsonError>(json!({ "doggo": null })).unwrap_err();
+
+    assert_debug_snapshot!(data, @r###"
+    JsonError(
+        "Json deserialize error: unknown field `doggo`, expected one of  at ``.",
+    )
+    "###);
+
+    let data = deserialize::<Struct, _, JsonError>(json!({ "doggo": "bork" })).unwrap_err();
+
+    assert_debug_snapshot!(data, @r###"
+    JsonError(
+        "Json deserialize error: unknown field `doggo`, expected one of  at ``.",
+    )
+    "###);
+}
+
+#[test]
+fn skip_and_default() {
+    #[allow(unused)]
+    #[derive(Debug, DeserializeFromValue)]
+    struct Struct {
+        #[deserr(skip, default = Some(String::from("bork")))]
+        doggo: Option<String>,
+    }
+
+    let data = deserialize::<Struct, _, JsonError>(json!({})).unwrap();
+
+    assert_debug_snapshot!(data, @r###"
+    Struct {
+        doggo: Some(
+            "bork",
+        ),
+    }
+    "###);
+
+    let data = deserialize::<Struct, _, JsonError>(json!({ "doggo": null })).unwrap();
+
+    assert_debug_snapshot!(data, @r###"
+    Struct {
+        doggo: Some(
+            "bork",
+        ),
+    }
+    "###);
+
+    let data = deserialize::<Struct, _, JsonError>(json!({ "doggo": "bork" })).unwrap();
+
+    assert_debug_snapshot!(data, @r###"
+    Struct {
+        doggo: Some(
+            "bork",
+        ),
+    }
+    "###);
+}
+
+#[test]
+fn skip_and_default_and_deny_unknown_fields() {
+    #[allow(unused)]
+    #[derive(Debug, DeserializeFromValue)]
+    #[deserr(deny_unknown_fields)]
+    struct Struct {
+        #[deserr(skip, default = Some(String::from("bork")))]
+        doggo: Option<String>,
+    }
+
+    let data = deserialize::<Struct, _, JsonError>(json!({})).unwrap();
+
+    assert_debug_snapshot!(data, @r###"
+    Struct {
+        doggo: Some(
+            "bork",
+        ),
+    }
+    "###);
+
+    let data = deserialize::<Struct, _, JsonError>(json!({ "doggo": null })).unwrap_err();
+
+    assert_debug_snapshot!(data, @r###"
+    JsonError(
+        "Json deserialize error: unknown field `doggo`, expected one of  at ``.",
+    )
+    "###);
+
+    let data = deserialize::<Struct, _, JsonError>(json!({ "doggo": "bork" })).unwrap_err();
+
+    assert_debug_snapshot!(data, @r###"
+    JsonError(
+        "Json deserialize error: unknown field `doggo`, expected one of  at ``.",
+    )
+    "###);
+}
